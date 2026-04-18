fe2128e4988a27f8a661b67fd94d5848a644e161
David Thomann <david.joshua.thomann@gmail.com>
2026-04-18 04:09:21 +0200

    Adjusted responsiveness

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,3 +1,15 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+
+const menuVisible = ref(false)
+
+function toggleMenu() {
+    menuVisible.value = !menuVisible.value
+}
+
+</script>
+
 <template>
     <div id="flexbox">
         <section id="name">davidthomann.dev</section>
@@ -6,18 +18,57 @@
             <a href="#about">About me</a>
             <a href="#contact">Contact</a>
         </section>
+
+        <section id="links-menu">
+            <div v-on:click="toggleMenu">Menu</div>
+        </section>
+
+        <Transition name="slide">
+            <div v-if="menuVisible" id="menu-flexbox">
+                <div><a href="#projects">Projects</a></div>
+                <div><a href="#about">About me</a></div>
+                <div><a href="#contact">Contact</a></div>
+            </div>
+        </Transition>
     </div>
+    
 </template>
 
 <style lang="css" scoped>
     #flexbox {
+        position: relative;
         width: 100%;
         height: 5vh;
         display: flex;
         align-items: center;
         justify-content: space-between;
+    }
 
-        
+    #menu-flexbox {
+        position: absolute;
+        top: 5vh;
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+    }
+
+    #menu-flexbox div {
+        margin-right: 10%;
+        display: flex;
+        align-items: center;
+        justify-content: flex-end;
+    }
+
+    .slide-enter-active,
+    .slide-leave-active {
+        transition: all 0.25s ease;
+    }
+
+    .slide-enter-from,
+    .slide-leave-to {
+        transform: translateY(-10px);
+        opacity: 0;
     }
 
     a {
@@ -29,6 +80,22 @@
         align-items: center;
         gap: 20px;
         margin-right: 10%;
+    }
+
+    #links-menu {
+        margin-right: 10%;
+        display: none;
+        color: rgb(112, 115, 145);
+    }
+
+    @media (max-width: 1024px) {
+        #links {
+            display: none;
+        }
+        #links-menu {
+            display: block
+        }
+        
     }
 
     #name {
